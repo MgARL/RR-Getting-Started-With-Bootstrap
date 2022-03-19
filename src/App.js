ac08b3d710942a16d92b81fe622b41d6350e7844
Miguel(MgARL) <mgarlcr@gmail.com>
2022-03-19 14:27:41 -0400

    Added React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+
+
 
 
 
@@ -6,7 +12,63 @@ function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+
+      <Nav activeKey={'/home'} variant='tabs'>
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey={'/about'}>About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <Form className='container'>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll  share your email with everyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as="select">
+            <option>Inception</option>
+            <option>Forrest Gump</option>
+            <option>Shawshank Redemption</option>
+            <option>Spiderman: Into the Spiderverse</option>
+            <option>Avengers: Endgame</option>
+          </Form.Control>
+        </Form.Group>
+
+
+        <Button className='my-3' variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+      <Card className='container mb-4' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
     </div>
   );
 }
